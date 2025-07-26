deba804af1d06312fd68389232b2cf9334f91e60
HumanEntity <human_entity@icloud.com>
2025-07-26 17:42:32 +0200

    Crude parser and composer working

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define META_IMPL
+#include "meta.h"
+
+const char *test_meta_source = "(\n"
+                               "\ttest: \"Data\"\n"
+                               "\tmore: 10\n"
+                               ")";
+
+int main ( void ) {
+        meta_value value = meta_parse_string( test_meta_source );
+
+        if ( value.type != META_VALUETYPE_OBJ ) {
+                fprintf( stderr, "Should be obj\n" );
+                return -1;
+        }
+
+        char buffer[1024] = { 0 };
+
+        meta_compose( value, buffer, sizeof( buffer ) );
+
+        printf( "Composed:\n%s\n", buffer );
+
+        return 0;
+}
